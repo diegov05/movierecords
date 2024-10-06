e770eacfa13666fabb3bf2e5cf50c06e5d8163fc
Diego Vargas <diegovs0504@gmail.com>
2024-10-06 16:00:52 -0400

    Modified Index and Default Layouts

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,28 @@
-import MovieCard from "@/components/movieCard";
+import HeroSection from "@/components/hero";
+import MovieCarousel from "@/components/movie-carousel";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <MovieCard />
+      <div className="flex flex-col gap-16">
+        <HeroSection />
+        <div className="flex flex-col gap-8">
+          <h1 className="text-3xl sm:text-5xl">What to Watch</h1>
+          <MovieCarousel
+            description="TV shows and movies just for you"
+            title="Top Picks"
+          />
+          <MovieCarousel
+            description="Movies and TV shows that you have watchlisted"
+            title="From your watchlist"
+          />
+          <MovieCarousel
+            description="This week's top TV and movies"
+            title="Fan favorites"
+          />
+        </div>
+      </div>
     </DefaultLayout>
   );
 }
