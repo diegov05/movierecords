2286db1b97f2045d4c0abe11066dafad84706f73
Diego Vargas <diegovs0504@gmail.com>
2024-10-07 17:58:32 -0400

    Distributed width in hero section

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -5,7 +5,9 @@ import MoviePreview from "./movie-preview";
 export default function HeroSection() {
   return (
     <section className="flex flex-row w-full pt-8 gap-4">
-      <MovieCard />
+      <div className="h-96 lg:h-auto w-full lg:w-[70%]">
+        <MovieCard />
+      </div>
       <div className="hidden flex-col gap-2 w-[30%] lg:flex">
         {[1, 2, 3].map((num) => (
           <MoviePreview key={num} />
