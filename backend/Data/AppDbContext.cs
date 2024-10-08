c9de146343ec9b39d4983fc946fce0d0efed564e
Diego Vargas <diegovs0504@gmail.com>
2024-10-08 19:14:21 -0400

    Created AppDbContext

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<UserRating> UserRatings { get; set; }
+        public DbSet<MovieRating> MovieRatings { get; set; }
+
+
+    }
+}
