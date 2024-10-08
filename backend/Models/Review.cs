25df67573c4c809dd82f33a1e46aa00ca2e67613
Diego Vargas <diegovs0504@gmail.com>
2024-10-08 19:14:13 -0400

    Created Review Model

---
+++ backend/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string UserId { get; set; }
+        public int Rating { get; set; }
+        public string Text { get; set; }
+        public string MovieId { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+
+    }
+}
